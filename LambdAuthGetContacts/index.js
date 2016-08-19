6cd2c869fac75da46c336c29147aeb4c97cfe7cc
Pablo Nussembaum <baunax@gmail.com>
2016-08-19 13:41:00 -0300

    add getContacts lambda

---
+++ LambdAuthGetContacts/index.js
@@ -0,0 +1,94 @@
+console.log('Loading function: GetContacts');
+
+// dependencies
+var AWS = require('aws-sdk');
+var crypto = require('crypto');
+var config = require('./config.json');
+
+// Get reference to AWS clients
+var dynamodb = new AWS.DynamoDB();
+
+function computeHash(password, salt, fn) {
+	// Bytesize
+	var len = 128;
+	var iterations = 4096;
+
+	if (3 == arguments.length) {
+		crypto.pbkdf2(password, salt, iterations, len, function(err, derivedKey) {
+			if (err) return fn(err);
+			else fn(null, salt, derivedKey.toString('base64'));
+		});
+	} else {
+		fn = salt;
+		crypto.randomBytes(len, function(err, salt) {
+			if (err) return fn(err);
+			salt = salt.toString('base64');
+			computeHash(password, salt, fn);
+		});
+	}
+}
+
+function getUser(email, fn) {
+	dynamodb.getItem({
+		TableName: config.DDB_TABLE,
+		Key: {
+			email: {
+				S: email
+			}
+		}
+	}, function(err, data) {
+		if (err) return fn(err);
+		else {
+			if ('Item' in data) {
+				var hash = data.Item.passwordHash.S;
+				var salt = data.Item.passwordSalt.S;
+				fn(null, hash, salt);
+			} else {
+				fn(null, null); // User not found
+			}
+		}
+	});
+}
+
+function updateUser(email, password, salt, fn) {
+	dynamodb.updateItem({
+			TableName: config.DDB_TABLE,
+			Key: {
+				email: {
+					S: email
+				}
+			},
+			AttributeUpdates: {
+				passwordHash: {
+					Action: 'PUT',
+					Value: {
+						S: password
+					}
+				},
+				passwordSalt: {
+					Action: 'PUT',
+					Value: {
+						S: salt
+					}
+				}
+			}
+		},
+		fn);
+}
+
+exports.handler = function(event, context) {
+	
+	context.succeed([
+ {
+   "id": "1",
+   "email": "pwolfus@gmail.com",
+   "name": "Pablo Wolfus"
+ },
+ {
+   "id": "2",
+   "email": "foo@foo.com",
+   "name": "foo foo"
+ }
+]);	
+
+}
